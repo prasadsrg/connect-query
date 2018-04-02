689b224c0d1fb2774e5777b89c56a683a7ab7ebd
Sachin Sharma <sachinsharma1057@gmail.com>
2018-04-02 12:53:57 +0530

    commit for tables

--- src/app/query/query.service.ts
+++ src/app/query/query.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { MysqlConnectionService } from '../common/mysql-connection.service';
+import { ConnectPageService } from '../connect-page/connect/connect-page.service';
 
 @Injectable()
 export class QueryService {
-
-  constructor(private mysqlConnectionService: MysqlConnectionService) {
-
+  private q:any;
+  constructor(private mysqlConnectionService: MysqlConnectionService, private connectService: ConnectPageService) {
+    
    }
 
    getTables(callback){
     this.mysqlConnectionService.get( (err, conn) =>{
+    var str;
+    str = this.connectService.getCard().schema;
+    this.q = "SELECT table_name FROM information_schema.tables WHERE table_schema='"+str+"'";
       if(err) throw err;
-      var query= conn.query("show tables", (err, rows) => {
+      var query= conn.query(this.q, (err, rows) => {
               if(err)  throw err;
               callback(JSON.parse(JSON.stringify(rows))) 
       });
